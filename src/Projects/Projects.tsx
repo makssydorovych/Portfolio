a4c9f35d3d4d363d608d9ff3ddd937ccf3d93bc9
Maksym <maksymsydorovych@gmail.com>
2023-11-17 00:23:19 +0100

    fix: refactor projects array

--- src/Projects/Projects.tsx
+++ src/Projects/Projects.tsx
@@ -8,41 +8,65 @@ import hotel from "../assets/images/holidaze.jpg";
 import kidsT from "../assets/images/kids-toys.jpg";
 import mus from "../assets/images/museum.jpg";
 
+const projectData = [
+	{
+		title: "Todolist",
+		img: { backgroundImage: `url(${todolist})` },
+		text: "view",
+		refs: "https://makssydorovych.github.io/todoList/",
+		description:
+			"Todolist - web-app which helping create todolists with tasks. Which you can mark and display completed and active points, add new and delete tasks and todolists. Also implemented authentication with remember user.",
+	},
+	{
+		title: "Social network",
+		img: { backgroundImage: "url(https://res.cloudinary.com/dkombzxne/image/upload/v1681205473/Social-network_slw3ed.png)" },
+		text: "view",
+		refs: "https://makssydorovych.github.io/social-network/",
+		description:
+			"Social network - web app with authentication, profile page, posts, audio player, chat. Built with React, Typescript, Redux, Redux thunk, Formik, Mui also using some class components and HOCs",
+	},
+	{
+		title: "Holidayz",
+		img: { backgroundImage: `url(${hotel})` },
+		text: "view",
+		refs: "https://makssydorovych.github.io/updated_for_portf/",
+		description: "Holidayz - is a fictional company which displays a number of different establishments in Bergen, Norway. Website built with React.Js/Sass",
+	},
+	{
+		title: "Comunity Sciense Museum",
+		img: { backgroundImage: `url(${mus})` },
+		text: "view",
+		refs: "https://mellow-rugelach-f45fa8.netlify.app/",
+		description:
+			"Comunity Sciense Museum - web site of a local museum aimed at a children's audience. Website responsive and have few pages. Site made using only html/css.",
+	},
+	{
+		title: "Kids-Toys",
+		img: { backgroundImage: `url(${kidsT})` },
+		text: "view",
+		refs: "https://grand-syrniki-dc20db.netlify.app/",
+		description: "Kids-Toys: e-commerce website with product cart and admin section, built using html, sass, bootstrap and js.",
+	},
+];
+
 const Projects = () => {
-	const museum = {
-		backgroundImage: `url(${mus})`,
-	};
-	const holidaze = {
-		backgroundImage: `url(${hotel})`,
-	};
-	const todo = {
-		backgroundImage: `url(${todolist})`,
-	};
-	const kids = {
-		backgroundImage: `url(${kidsT})`,
-	};
-	const social = {
-		backgroundImage:"url(https://res.cloudinary.com/dkombzxne/image/upload/v1681205473/Social-network_slw3ed.png)"};
 	return (
 		<div id="projects" className={style.projectsBlock}>
-
 			<div className={`${styleContainer.container} ${style.projectsContainer}`}>
 				<Title title={"Projects"} />
 				<div className={style.projects}>
-					<Project img={todo} title={"Todolist"} text={'view'} description={'Todolist - web-app which helping create todolists with tasks.' +
-						' Which you can mark and display completed and active points, add new and delete tasks and todolists.' +
-						'Also implemented authentication with remember user.'} refs={'https://makssydorovych.github.io/todoList/'}/>
-					<Project img={social} title={"Social network"} text={'view'} refs={'https://makssydorovych.github.io/social-network/'} description={'Social network - web app with authentication, ' +
-						'profile page, posts, audio player, chat.Built with React,Typescript,Redux, Redux thunk,Formik,Mui also using some class components and HOCs '}/>
-					{/*<Project img={holidaze} title={"Title"} text={'view'} refs={'https://makssydorovych.github.io/todoList/'}/>*/}
-					<Project img={holidaze} title={"Title"} text={'view'} refs={'https://makssydorovych.github.io/updated_for_portf/'} description={'Holidayz - is a fictional company which displays a number of different establishments in Bergen, Norway. Website built with React.Js/Sass'}/>
-					<Project img={museum} title={"Comunity Sciense Museum"} description={'Comunity Sciense Museum - web site of a local museum aimed at a children\'s audience.' +
-						'Website responsive and have few pages.' +
-						'Site made using only html/css. '} text={'view'} refs={'https://mellow-rugelach-f45fa8.netlify.app/'}/>
-					<Project img={kids} title={"Kids-Toys"} text={'view'}  refs={'https://grand-syrniki-dc20db.netlify.app/'} description={'Kids-Toys: e-commerce website with product cart and admin section, built using html,sass,bootstrap and js.'}/>
+					{projectData.map((project, index) => (
+						<Project
+							key={index}
+							img={project.img}
+							title={project.title}
+							text={project.text}
+							refs={project.refs}
+							description={project.description}
+						/>
+					))}
 				</div>
 			</div>
-
 		</div>
 	);
 };
